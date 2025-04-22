d3cfdbf076541562798f520a82960ba3cfd7d8d2
Buggy-Sama0 <157221907+Buggy-Sama0@users.noreply.github.com>
2025-04-22 23:27:56 +0800

    Print result

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/show', (req, res) => {
         (err, result)=> {
             if (err) console.log(err);     
             
-
+            console.log(result);
             res.status(200).json(result);
         }
     )
